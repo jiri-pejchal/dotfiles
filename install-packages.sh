d506d1a462c6bf100ad6b6cd6b6284e56e359787
Jiří Pejchal <jiri.pejchal@gmail.com>
2025-03-22 13:46:11 +0100

    List packages to install

---
+++ install-packages.sh
@@ -0,0 +1,11 @@
+# CLI apps
+sudo apt-get install git curl wget zoxide
+
+# development
+sudo apt-get install neovim
+
+# gui
+sudo apt-get install flameshot
+
+# media
+sudo apt-get install audacity mpv yt-dlp
